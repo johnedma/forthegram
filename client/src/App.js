e8f97ad9d7e1909056fbe1cdc5da5e1a4b42c20d
johned <johned.matt@gmail.com>
2020-10-23 01:29:41 -0500

    added navbar component with fixed top css

--- client/src/App.js
+++ client/src/App.js
@@ -1,30 +1,32 @@
 import React from 'react';
 import { BrowserRouter, Switch, Route, NavLink } from 'react-router-dom';
+import Navbar from './components/Navbar';
 
 import UserList from './components/UsersList';
 
 
 function App() {
 
-  return (
-    <BrowserRouter>
-        <nav>
-            <ul>
-                <li><NavLink to="/" activeclass="active">Home</NavLink></li>
-                <li><NavLink to="/users" activeclass="active">Users</NavLink></li>
-            </ul>
-        </nav>
-        <Switch>
-            <Route path="/users">
-                <UserList />
-            </Route>
+    return (
+        <BrowserRouter>
+            <Navbar />
+            <nav>
+                <ul>
+                    <li><NavLink to="/" activeclass="active">Home</NavLink></li>
+                    <li><NavLink to="/users" activeclass="active">Users</NavLink></li>
+                </ul>
+            </nav>
+            <Switch>
+                <Route path="/users">
+                    <UserList />
+                </Route>
 
-            <Route path="/">
-                <h1>My Home Page</h1>
-            </Route>
-        </Switch>
-    </BrowserRouter>
-  );
+                <Route path="/">
+                    <h1>My Home Page</h1>
+                </Route>
+            </Switch>
+        </BrowserRouter>
+    );
 }
 
 export default App;
---
+++ client/src/components/Navbar.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { NavLink } from 'react-router-dom';
+
+const Navbar = () =>
+    <nav className="navbar is-fixed-top">
+        <div className="navbar-brand">
+            <NavLink to="/" activeClassName="active"><h1>forthegram</h1></NavLink>
+        </div>
+        <div className="navbar-menu is-active">
+            <div className="navbar-end">
+
+                <div className="navbar-item">
+                    {/*  */}
+                </div>
+            </div>
+        </div>
+    </nav>
+
+export default Navbar;
