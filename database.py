1a980557f64a007b1f92082cf40ca6766f059568
Peter Knipp <peteraknipp@gmail.com>
2020-10-24 11:59:25 -0400

    Create User model, seed User model, and create Post model.  (Follow model is buggy.)

--- database.py
+++ database.py
@@ -1,25 +1,19 @@
 from dotenv import load_dotenv
+from datetime import date, datetime
 load_dotenv()
 
 from starter_app import app, db
 from starter_app.models import User
 
 with app.app_context():
-  db.drop_all()
-  db.create_all()
-
-  ian = User(username = 'Ian', email = 'ian@aa.io')
-  javier = User(username = 'Javier', email = 'javier@aa.io')
-  dean = User(username = 'Dean', email = 'dean@aa.io')
-  angela = User(username = 'Angela', email = 'angela@aa.io')
-  soonmi = User(username = 'Soon-Mi', email = 'soonmi@aa.io')
-  alissa = User(username = 'Alissa', email = 'alissa@aa.io')
-
-  db.session.add(ian)
-  db.session.add(javier)
-  db.session.add(dean)
-  db.session.add(angela)
-  db.session.add(soonmi)
-  db.session.add(alissa)
-
-  db.session.commit()
+    db.drop_all()
+    db.create_all()
+    db.session.add(User(
+      user_name="demo_user",
+      first_name="Demo",
+      last_name="User",
+      DOB=date(2020, 10, 31),
+      created_at=datetime.now(),
+      updated_at=datetime.now()
+      ))
+    db.session.commit()
--- starter_app/models.py
+++ starter_app/models.py
@@ -1,17 +1,78 @@
 from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 db = SQLAlchemy()
 
+
 class User(db.Model):
-  __tablename__ = 'users'
+    __tablename__ = 'users'
 
-  id = db.Column(db.Integer, primary_key = True)
-  username = db.Column(db.String(40), nullable = False, unique = True)
-  email = db.Column(db.String(255), nullable = False, unique = True)
+    id = db.Column(db.Integer, primary_key=True)
+    user_name = db.Column(db.String(40), nullable=False, unique=True)
+    first_name = db.Column(db.String(40))
+    last_name = db.Column(db.String(40))
+    DOB = db.Column(db.Date, nullable=False)
+    created_at = db.Column(db.DateTime, nullable=False)
+    updated_at = db.Column(db.DateTime, nullable=False)
 
-  def to_dict(self):
-    return {
-      "id": self.id,
-      "username": self.username,
-      "email": self.email
-    }
+    posts = db.relationship("Post", back_populates="user")
+    # follows = db.relationship("Follow", back_populates="user")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "user_name": self.user_name,
+            "first_name": self.first_name,
+            "last_name": self.last_name,
+            "DOB": self.DOB,
+            "created_at": this.created_at,
+            "updated_at": this.updated_at
+        }
+
+
+class Post(db.Model):
+    __tablename__ = 'posts'
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(
+        db.Integer, db.ForeignKey("users.id"), nullable=False
+        )
+    photo_url = db.Column(db.String(80), unique=True)
+    caption = db.Column(db.String(40))
+    created_at = db.Column(db.DateTime, nullable=False)
+    updated_at = db.Column(db.DateTime, nullable=False)
+
+    user = db.relationship("User", back_populates="posts")
+
+    def to_dict(self):
+        return {
+            "id": self.id,
+            "user_id": self.user_id,
+            "photo_url": self.photo_url,
+            "caption": self.caption,
+            "created_at": this.created_at,
+            "updated_at": this.updated_at
+        }
+
+
+# class Follow(db.Model):
+#     __tablename__ = 'follows'
+
+#     id = db.Column(db.Integer, primary_key=True)
+#     follower_id = db.Column(
+#         db.Integer, db.ForeignKey("users.id"), nullable=False
+#         )
+#     followed_id = db.Column(
+#         db.Integer, db.ForeignKey("users.id"), nullable=False
+#         )
+#     created_at = db.Column(db.DateTime, nullable=False)
+
+#     user = db.relationship("User", back_populates="follows")
+
+#     def to_dict(self):
+#         return {
+#             "id": self.id,
+#             "follower_id": self.follower_id,
+#             "followed_id": self.followed_id,
+#             "created_at": this.created_at,
+#         }
