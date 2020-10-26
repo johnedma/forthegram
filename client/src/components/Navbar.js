15bf2bca1e729547678dc26da226ea6c64aadadf
johned <johned.matt@gmail.com>
2020-10-26 11:16:13 -0500

    updated nav name to Petstagram

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -4,7 +4,7 @@ import { NavLink } from 'react-router-dom';
 const Navbar = () =>
     <nav className="navbar is-fixed-top" style={{ border: `1px solid #efefef` }}>
         <div className="navbar-brand" style={{ alignItems: `center` }}>
-            <NavLink to="/" activeClassName="active"><h1 style={{ alignItems: `center`, fontSize: `24px` }}>DeadPoetstagram</h1></NavLink>
+            <NavLink to="/" activeClassName="active"><h1 style={{ alignItems: `center`, fontSize: `24px` }}>Petstagram</h1></NavLink>
         </div>
         <div className="navbar-menu is-active">
             <div className="navbar-end">
