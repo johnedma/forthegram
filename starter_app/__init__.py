787aefad644f540f8944bed6b1600b5846ed06cb
Aaron Cygnarowicz <aaroncygnarowicz@Aarons-MacBook-Pro.local>
2020-10-26 17:11:08 -0400

    added alembic and flask migrate

--- starter_app/__init__.py
+++ starter_app/__init__.py
@@ -2,10 +2,12 @@ import os
 from flask import Flask, render_template, request, session
 from flask_cors import CORS
 from flask_wtf.csrf import CSRFProtect, generate_csrf
+from flask_migrate import Migrate
 
 
 from starter_app.models import db, User
 from starter_app.api.user_routes import user_routes
+# from starter_app.api.posts import posts
 
 from starter_app.config import Config
 
@@ -13,17 +15,22 @@ app = Flask(__name__)
 
 app.config.from_object(Config)
 app.register_blueprint(user_routes, url_prefix='/api/users')
+# app.register_blueprint(posts, url_prefix='api/posts')
 db.init_app(app)
+Migrate(app, db)
 
-## Application Security
+# Application Security
 CORS(app)
+
+
 @app.after_request
 def inject_csrf_token(response):
     response.set_cookie('csrf_token',
-        generate_csrf(),
-        secure=True if os.environ.get('FLASK_ENV') else False,
-        samesite='Strict' if os.environ.get('FLASK_ENV') else None,
-        httponly=True)
+                        generate_csrf(),
+                        secure=True if os.environ.get('FLASK_ENV') else False,
+                        samesite='Strict' if os.environ.get(
+                            'FLASK_ENV') else None,
+                        httponly=True)
     return response
 
 
---
+++ starter_app/api/posts.py
@@ -0,0 +1,6 @@
+from flask import Blueprint
+
+bp = Blueprint('posts', __name__, '/api/posts')
+
+
+# @bp.route('/:id', )
