5ebd2e601b7a8913f91c3fb28417096b5dc1984a
Tyna William <tynawilliam@gmail.com>
2020-10-30 14:37:21 -0400

    fixed POST to following

--- petstagram/api/following.py
+++ petstagram/api/following.py
@@ -8,20 +8,33 @@ import time
 following = Blueprint('following', __name__)
 
 #get list of all following
-@following.route('/<int:id>', methods=['GET'])
+@following.route('/<int:id>', methods=['GET', 'POST'])
 def index(id):
     uid = int(id)
-    get_following = Follow.query.filter(Follow.follower_id == uid).all()
-    following_list = [following.followed_id for following in get_following]
-    post_list = [
-        Post.query.filter(Post.user_id == fid).order_by(Post.created_at.desc()).all()
-        for fid in following_list
-    ]
-    pid_list = [
-        [pid.id for post in post_list for pid in post]
-    ]
+    if request.method == 'GET':
+        get_following = Follow.query.filter(Follow.follower_id == uid).all()
+        following_list = [following.followed_id for following in get_following]
+        post_list = [
+            Post.query.filter(Post.user_id == fid).order_by(Post.created_at.desc()).all()
+            for fid in following_list
+        ]
+        pid_list = [
+            [pid.id for post in post_list for pid in post]
+        ]
+        return {"following": following_list, "follower-posts": pid_list}
 
-    return {"following": following_list, "follower-posts": pid_list}
+    if request.method == 'POST':
+        followed_id = request.form['uid']
+
+        new_follow = Follow(
+                            follower_id=uid,
+                            followed_id=followed_id,
+                            created_at=datetime.now()
+        )
+        db.session.add(new_follow)
+        db.session.commit()
+        return 'posted'
+
 #get user_id of following
 #get list of posts with followers user ids
 #sort posts by most recent created at date
